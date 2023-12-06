eee51aaa38d7e0e42d3ae6dcf3bae2a19fe98308
captain-meliodas <38395296+captain-meliodas@users.noreply.github.com>
2023-12-06 19:44:54 +0530

    chore(leetcode): linked list cycle sol 2

--- Week1/linkedlistcycle.py
+++ Week1/linkedlistcycle.py
@@ -6,6 +6,7 @@
 #         self.val = x
 #         self.next = None
 
+#Solution 1
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         temp = head
@@ -15,6 +16,26 @@ class Solution:
                 return True
             temp.val = 'v'
             temp = temp.next
+        
+        return False
+
+
+#Solution 2
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slowp = fastp = head
+
+        while fastp and fastp.next:
+            fastp = fastp.next.next
+            slowp = slowp.next
+            if fastp == slowp:
+                return True
         
         return False
 
