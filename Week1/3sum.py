1a7a3490b08ce6910d8e5acad690f3d1c6cc0016
captain-meliodas <38395296+captain-meliodas@users.noreply.github.com>
2024-02-22 22:52:38 +0530

    added comments for 3 and 2 sum

--- Week1/3sum.py
+++ Week1/3sum.py
@@ -1,12 +1,14 @@
 # Link -> https://leetcode.com/problems/3sum/
 
+# input = [-1,0,1,2,-1,-4]
+
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        nums.sort() # sort if sum is less than 0 we move left+1 and if greater than 0 we move from right-1[-4,-1,-1,0,1,2]
         n = len(nums)
         result = []
-        for i,a in enumerate(nums):
-            if i>0 and a == nums[i-1]:
+        for i,a in enumerate(nums): # get index and value from array list
+            if i>0 and a == nums[i-1]: #ignore if we get same number on continuous move
                 continue
             left = i + 1
             right = n - 1
@@ -19,12 +21,12 @@ class Solution:
                 elif _sum > 0:
                     right -= 1
                 else:
-                    result.append([a,b,c])
+                    result.append([a,b,c]) #if sum is 0 append values
                     left += 1
-                    while left < right and nums[left] == b:
+                    while left < right and nums[left] == b: #skip all left if left+1 is equal to left till we reaches right
                         left += 1
                     
                     right -= 1
-                    while left < right and nums[right] == c:
+                    while left < right and nums[right] == c: #skip all right if right-1 is equal to right till we reaches left
                         right -= 1
         return result
--- Week1/twosum.py
+++ Week1/twosum.py
@@ -4,7 +4,7 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prevMap = {}
         for i,n in enumerate(nums):
-            diff = target - n
-            if diff in prevMap:
-                return [prevMap[diff],i]
-            prevMap[n] = i
+            diff = target - n #get the difference
+            if diff in prevMap: #check if difference is present in map keys
+                return [prevMap[diff],i] #return index of diff and current index
+            prevMap[n] = i #otherwise add diff as key and value as index
